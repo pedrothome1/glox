ddc740cf1583a555c4052b0ddb0408bdda5f95f6
pedrothome1 <pedrothome2@outlook.com>
2023-09-04 07:53:32 -0300

    implemented variable resolver
    TODO: implement interpreter.Resolve(...)
    
    bookmark: https://craftinginterpreters.com/resolving-and-binding.html#interpreting-resolved-variables

--- resolver.go
+++ resolver.go
@@ -5,14 +5,23 @@ type resolver struct {
 	scopes      mapStack
 }
 
+// region statements
 func (r *resolver) VisitExpressionStmt(stmt ExpressionStmt) error {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(stmt.Expression)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *resolver) VisitPrintStmt(stmt PrintStmt) error {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(stmt.Expression)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *resolver) VisitVarStmt(stmt VarStmt) error {
@@ -44,43 +53,100 @@ func (r *resolver) VisitBlockStmt(stmt BlockStmt) error {
 }
 
 func (r *resolver) VisitIfStmt(stmt IfStmt) error {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(stmt.Condition)
+	if err != nil {
+		return err
+	}
+
+	err = r.resolveStmt(stmt.ThenBranch)
+	if err != nil {
+		return err
+	}
+
+	if stmt.ElseBranch != nil {
+		err = r.resolveStmt(stmt.ElseBranch)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (r *resolver) VisitWhileStmt(stmt WhileStmt) error {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(stmt.Condition)
+	if err != nil {
+		return err
+	}
+
+	err = r.resolveStmt(stmt.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *resolver) VisitFunctionStmt(stmt FunctionStmt) error {
-	//TODO implement me
-	panic("implement me")
+	r.declare(stmt.Name)
+	r.define(stmt.Name)
+
+	err := r.resolveFunction(stmt)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *resolver) VisitReturnStmt(stmt ReturnStmt) error {
-	//TODO implement me
-	panic("implement me")
+	if stmt.Value != nil {
+		err := r.resolveExpr(stmt.Value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
+// endregion
+
+// region expressions
 func (r *resolver) VisitBinaryExpr(expr Binary) (any, error) {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(expr.Left)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.resolveExpr(expr.Right)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
 
 func (r *resolver) VisitGroupingExpr(expr Grouping) (any, error) {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(expr.Expression)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
 
 func (r *resolver) VisitLiteralExpr(expr Literal) (any, error) {
-	//TODO implement me
-	panic("implement me")
+	return nil, nil
 }
 
 func (r *resolver) VisitUnaryExpr(expr Unary) (any, error) {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(expr.Right)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
 
 func (r *resolver) VisitVariableExpr(expr Variable) (any, error) {
@@ -108,15 +174,38 @@ func (r *resolver) VisitAssignExpr(expr Assign) (any, error) {
 }
 
 func (r *resolver) VisitLogicalExpr(expr Logical) (any, error) {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(expr.Left)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.resolveExpr(expr.Right)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
 
 func (r *resolver) VisitCallExpr(expr Call) (any, error) {
-	//TODO implement me
-	panic("implement me")
+	err := r.resolveExpr(expr.Callee)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, arg := range expr.Arguments {
+		err = r.resolveExpr(arg)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, nil
 }
 
+// endregion
+
+// region helpers
 func (r *resolver) resolveStmts(statements []Stmt) error {
 	for _, statement := range statements {
 		err := r.resolveStmt(statement)
@@ -175,7 +264,27 @@ func (r *resolver) define(name Token) {
 	scope[name.Lexeme] = true
 }
 
-// helper data structures
+func (r *resolver) resolveFunction(fn FunctionStmt) error {
+	r.beginScope()
+
+	for _, param := range fn.Params {
+		r.declare(param)
+		r.define(param)
+	}
+
+	err := r.resolveStmts(fn.Body)
+	if err != nil {
+		return err
+	}
+
+	r.endScope()
+
+	return nil
+}
+
+// endregion
+
+// region helper data structures
 type mapStack []map[string]bool
 
 func (s *mapStack) Push(m map[string]bool) {
@@ -200,3 +309,5 @@ func (s *mapStack) Peek() map[string]bool {
 
 	return nil
 }
+
+// endregion
