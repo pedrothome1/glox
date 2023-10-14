b631fa8e68bd31522852385d36b521c3dd12c53b
pedrothome1 <pedrothome2@outlook.com>
2023-10-13 21:12:14 -0300

    start implementing common modules
    bookmark: https://craftinginterpreters.com/chunks-of-bytecode.html#line-information

--- c/chunk.h
+++ c/chunk.h
@@ -2,9 +2,26 @@
 #define C_CHUNK_H
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t *code;
+    ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+
+void writeChunk(Chunk *chunk, uint8_t byte);
+
+int addConstant(Chunk *chunk, Value value);
+
+void freeChunk(Chunk *chunk);
 
 #endif
---
+++ c/debug.h
@@ -0,0 +1,9 @@
+#ifndef C_DEBUG_H
+#define C_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk *chunk, const char *name);
+int disassembleInstruction(Chunk *chunk, int offset);
+
+#endif
