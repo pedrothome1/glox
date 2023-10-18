1f8cc75a26d66add92df90fcc842bac02069f7b7
pedrothome1 <pedrothome2@outlook.com>
2023-10-17 21:55:13 -0300

    implemented + - * / operators
    bookmark: https://craftinginterpreters.com/scanning-on-demand.html

--- c/main.c
+++ c/main.c
@@ -9,9 +9,27 @@ int main(int argc, const char *argv[]) {
     Chunk chunk;
     initChunk(&chunk);
 
+    // My notes:
+    // ===================================================
+    // infix notation               -> (1.2 + 3.4) / 5.6
+    // cambridge polish notation    -> (/ (+ 1.2 3.4) 5.6)
+    // reverse polish notation      -> 1.2 3.4 + 5.6 /
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
     writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
