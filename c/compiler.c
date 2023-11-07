df0d37adf0d24737e2639b93810540243e80df7e
pedrothome1 <pedrothome2@outlook.com>
2023-11-07 08:43:09 -0300

    implemented print statement
    bookmark: https://craftinginterpreters.com/global-variables.html#expression-statements

--- c/compiler.c
+++ c/compiler.c
@@ -92,6 +92,16 @@ static void consume(TokenType type, const char *message) {
     errorAtCurrent(message);
 }
 
+static bool check(TokenType type) {
+    return parser.current.type == type;
+}
+
+static bool match(TokenType type) {
+    if (!check(type)) return false;
+    advance();
+    return true;
+}
+
 static void emitByte(uint8_t byte) {
     writeChunk(currentChunk(), byte, parser.previous.line);
 }
@@ -130,12 +140,32 @@ static void endCompiler() {
 
 static void expression();
 
+static void statement();
+
+static void declaration();
+
 static ParseRule *getRule(TokenType type);
 
 static void parsePrecedence(Precedence precedence);
 
 static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void printStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after value.");
+    emitByte(OP_PRINT);
+}
+
+static void declaration() {
+    statement();
+}
+
+static void statement() {
+    if (match(TOKEN_PRINT)) {
+        printStatement();
+    }
 }
 
 static void binary() {
@@ -300,8 +330,11 @@ bool compile(const char *source, Chunk *chunk) {
     parser.panicMode = false;
 
     advance();
-    expression();
-    consume(TOKEN_EOF, "Expect end of expression.");
+
+    while (!match(TOKEN_EOF)) {
+        declaration();
+    }
+
     endCompiler();
     return !parser.hadError;
 }
